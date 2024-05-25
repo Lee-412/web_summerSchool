621940838c4e45eb1b644899218a56472a67d942
LeHien2818 <hienltv281823@gmail.com>
2024-05-25 19:14:18 +0700

    done feature UI

--- src/Component/cardBase/card.tsx
+++ src/Component/cardBase/card.tsx
@@ -3,22 +3,37 @@
 import { Card, CardActions, CardContent, CardHeader, CardMedia, Collapse, IconButton, Typography } from "@mui/material";
 
 
-export default function ReviewCard({cardInfo}:any) {
+export default function ReviewCard(cardInfo:any) {
 
-
+   // console.log(cardInfo.card_info)
+    
 
     return (
-        <Card sx={{ maxWidth: 345, backgroundColor: "#007ec5" }}>
+        <Card sx=
+            {{ 
+                width: 300,
+                backgroundColor: "#007ec5" ,
+                borderRadius: 5,
+                marginBottom: 6
+             }}>
 
             <CardMedia
                 component="img"
                 height="194"
-                image={cardInfo.img}
+                image={cardInfo.card_info.img}
                 alt="Paella dish"
+                sx={{
+                    objectFit: "cover",
+                    minWidth: 300,
+                    borderRadius: 5
+                }}
             />
             <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                    {cardInfo.title}
+                <Typography variant="body2" color="white"
+                sx={{
+                    textAlign: "center"
+                }}>
+                    {cardInfo.card_info.title}
                 </Typography>
             </CardContent>
 
--- src/Component/homeCoursesComponent/mainCourses.tsx
+++ src/Component/homeCoursesComponent/mainCourses.tsx
@@ -1,14 +1,26 @@
 import { Box, Container } from "@mui/material";
 import SearchBase from "../searchBase/search";
 import ReviewCard from "../cardBase/card";
+import { log } from "console";
 const HomeCourses = async() => {
     const res = await fetch(`${process.env.STRAPI_LINK_API_URL}/courses?populate=*`, {cache: "no-store"})
     const courses = await res.json()
-    console.log(courses);
+    // console.log(courses);
+    let cards : any[] = []
     courses.data.map((course :any)=>{
         console.log(course.attributes.Thumbnail.data.attributes.url);
-        console.log(course.attributes);
+        //console.log(course.attributes);
+        let img_url = `${process.env.STRAPI_LINK_URL}${course.attributes.Thumbnail.data.attributes.url}`
+        let title = `${course.attributes.name}`
+        let card_info = 
+        {
+            img: img_url,
+            title: title,
+        }
+        cards.push(card_info)
     })
+    //console.log(cards);
+    
     return (
         <Box
 
@@ -22,7 +34,7 @@ const HomeCourses = async() => {
                 justifyContent: "space-between",
                 alignContent: "center",
                 alignItems: "center",
-                paddingBottom: "8%"
+                paddingBottom: "5%"
             }}>
             {/* <p style={{ height: "30px" }}></p> */}
             <SearchBase color={"#FFFFFF"} />
@@ -34,13 +46,14 @@ const HomeCourses = async() => {
                 flexWrap: "wrap",
                 paddingBottom:"5%"
             }}>
-                {}
-                <ReviewCard />
-                <ReviewCard />
-                <ReviewCard />
-                <ReviewCard />
-                <ReviewCard />
-                <ReviewCard />
+                {
+                    cards.map((card :any)=>{
+                        return(
+                            <ReviewCard card_info = {card} />
+                            
+                        )
+                    })
+                }
             </Box>
             <h2 style={{color: "white"}}>Upcoming Courses...</h2>
         </Box>
