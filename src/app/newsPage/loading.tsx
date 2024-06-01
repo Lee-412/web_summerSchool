5184c8e664ff453ab35a28b6ffa639854326ad6a
LeHien2818 <hienltv281823@gmail.com>
2024-06-01 10:39:48 +0700

    update loading UI

---
+++ src/app/newsPage/loading.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './loading.css'
+const loading = () => {
+  return (
+    <div className='loading-ui'>
+        <a href="https://azpivi.net/code-css-html-animation-loading/">
+            <section className="loading">
+                <div className="one"></div>
+                <p>Waiting for a while...</p>
+            </section>
+        </a>
+    </div>
+  )
+}
+
+export default loading
